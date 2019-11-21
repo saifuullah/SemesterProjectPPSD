ad32a3039f530f3c07be9e4809fd4e3a3e27457d
saifuullah <kk772578@gmail.com>
2019-11-21 03:53:32 -0800

    version 4 ::saif..

--- main.cpp
+++ main.cpp
@@ -4,9 +4,11 @@
 //AND THEIR SETTER AND GETTER
 
 
+#include <iostream>
+#include <string>
+#include <stdio.h>
 
-
-
+using namespace std;
 class customer
 {
   private:
@@ -175,3 +177,64 @@ string getShowTime()
 
 
 };
+
+
+
+/*********************************
+*******************************************
+***************************************************/
+
+
+//This is Class Function
+//This Class contain all the methods that will be used for different operations
+//We will make 1000 objects of customer class , it mean maximmum users will be 1000
+//We will make 20 objects of Class Movies so that Maximumm numbers of movies will be stored is 20
+//There is also some varibles used in this class
+//These Variables are defined at the start of the class in private section
+//These functions are called from the main function and it will take data from user and send it to the other function
+//         in other classes
+
+//Example : This class will work as a bridge
+
+// User->Main--call-->Functions-->Customer-data-->BookingSystem.
+
+class functions{
+public:
+    //Max of 1000 customers
+customer cus[1000];
+
+//Indexer for movie array
+int cus_current_place=0;
+
+
+//object of booking system
+BookingSystem  bookingSysObj[20];
+
+//This is movie stored limit
+const int movie_limit=20;
+
+//This will stored how many movies are stored
+int movie_stored=0;
+
+//This will hold current index for the customer data to be entered ::
+int current_place=0;
+
+
+
+
+
+
+
+
+
+
+
+};
+
+int main()
+{
+
+
+
+    return 0;
+}
